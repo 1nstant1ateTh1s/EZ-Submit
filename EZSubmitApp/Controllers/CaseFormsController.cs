417e9bd7a065d20ed0eb71ba06809358674e703d
Andrew Brown <ambrown@cityofchesapeake.net>
2020-08-25 20:13:22 +0000

    Implement CaseFormsController action methods (using _context directly).

--- EZSubmitApp/Controllers/CaseFormsController.cs
+++ EZSubmitApp/Controllers/CaseFormsController.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Bibliography;
+using EZSubmitApp.Core.Constants;
 using EZSubmitApp.Core.Entities;
+using EZSubmitApp.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -15,43 +19,108 @@ namespace EZSubmitApp.Controllers
     public class CaseFormsController : ControllerBase
     {
         private readonly ILogger _logger;
+        private readonly EZSubmitDbContext _context;
 
         public CaseFormsController(
-            ILogger<CaseFormsController> logger)
+            ILogger<CaseFormsController> logger,
+            EZSubmitDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        // GET: api/<CaseFormsController>
+        // GET: api/CaseForms
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<IEnumerable<CaseForm>>> Get()
         {
-            return new string[] { "value1", "value2" };
+            _logger.LogInformation(LoggingEvents.ListItems, "Listing all case forms from database.");
+
+            return await _context.CaseForms.ToListAsync();
         }
 
-        // GET api/<CaseFormsController>/5
+        // GET: api/CaseForms/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<CaseForm>> Get(int id)
         {
-            return "value";
+            _logger.LogInformation(LoggingEvents.GetItem, "Getting case form {Id}", id);
+
+            var caseForm = await _context.CaseForms.FindAsync(id);
+            if (caseForm == null)
+            {
+                _logger.LogWarning(LoggingEvents.GetItemNotFound, "Case form {Id} NOT FOUND", id);
+                return NotFound();
+            }
+
+            return caseForm;
         }
 
-        // POST api/<CaseFormsController>
+        // POST: api/CaseForms
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<CaseForm>> Post(CaseForm caseForm)
         {
+            _context.CaseForms.Add(caseForm);
+            await _context.SaveChangesAsync();
+            _logger.LogInformation(LoggingEvents.InsertItem, "Case form {Id} created", caseForm.Id);
+
+            return CreatedAtAction("Get", new { id = caseForm.Id }, caseForm);
         }
 
-        // PUT api/<CaseFormsController>/5
+        // PUT: api/CaseForms/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put(int id, CaseForm caseForm)
         {
+            if (id != caseForm.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(caseForm).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+                _logger.LogInformation(LoggingEvents.UpdateItem, "Case form {Id} Updated", caseForm.Id);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CaseFormExists(id))
+                {
+                    _logger.LogWarning(LoggingEvents.UpdateItemNotFound, "UPDATE FAILED: Case form {Id} no longer exists", caseForm.Id);
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
-        // DELETE api/<CaseFormsController>/5
+        // DELETE: api/CaseForms/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult<CaseForm>> Delete(int id)
         {
+            var caseForm = await _context.CaseForms.FindAsync(id);
+            if (caseForm == null)
+            {
+                _logger.LogWarning(LoggingEvents.DeleteItemNotFound, "Case form {Id} NOT FOUND", id);
+                return NotFound();
+            }
+
+            _context.CaseForms.Remove(caseForm);
+            await _context.SaveChangesAsync();
+            _logger.LogInformation(LoggingEvents.DeleteItem, "Case form {Id} deleted", id);
+
+            return caseForm;
+        }
+
+        private bool CaseFormExists(int id)
+        {
+            return _context.CaseForms.Any(e => e.Id == id);
         }
     }
 }
