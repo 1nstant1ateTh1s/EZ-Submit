3fab61054768ce395d632832937180caa61f766b
Andrew Brown <ambrown@cityofchesapeake.net>
2020-08-31 21:46:18 +0000

    Add AutoMapper to Core project

---
+++ EZSubmitApp.Core/Mapper/ObjectMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+
+namespace EZSubmitApp.Core.Mapper
+{
+    public static class ObjectMapper
+    {
+        private static readonly Lazy<IMapper> Lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                // TODO: add profiles
+            });
+            var mapper = config.CreateMapper();
+            return mapper;
+        });
+
+        public static IMapper Mapper => Lazy.Value;
+    }
+}
