0880c8cdd44e5b0ada6c0d5eda6b19d9715f7084
Andrew Brown <ambrown@cityofchesapeake.net>
2020-09-11 22:23:37 +0000

    Added custom value resolvers to map AccountType property.

--- EZSubmitApp.Core/Mapper/Profiles/CaseFormsProfile.cs
+++ EZSubmitApp.Core/Mapper/Profiles/CaseFormsProfile.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using DocxConverterService.Models;
 using EZSubmitApp.Core.Mapper.Converters;
+using EZSubmitApp.Core.Mapper.Resolvers;
 
 namespace EZSubmitApp.Core.Mapper.Profiles
 {
@@ -36,6 +38,18 @@ namespace EZSubmitApp.Core.Mapper.Profiles
 
             #region Docx conversion mappings
             // TODO 
+            CreateMap<Entities.WarrantInDebtForm, WarrantInDebtDocxFormFields>()
+                //.ForMember(dest => dest.Court, opt => opt.MapFrom("CHESAPEAKE")) // TODO: READ FROM CONFIGURATION
+                //.ForMember(dest => dest.CourtAddress, opt => opt.MapFrom("307 Albemarle Drive, Suite 200B, Chesapeake, VA 23322, PH-757-382-3143 FAX-757-382-3113")) // TODO: READ FROM CONFIGURATION
+                .ForMember(dest => dest.HearingDate, opt => opt.MapFrom(src => src.HearingDateTime.ToString("MM/dd/yyyy")))
+                .ForMember(dest => dest.HearingTime, opt => opt.MapFrom(src => src.HearingDateTime.ToString("hh:mm tt")))
+                .ForMember(dest => dest.A1, opt => opt.MapFrom<A1TypeResolver, string>(src => src.AccountType))
+                .ForMember(dest => dest.A2, opt => opt.MapFrom<A2TypeResolver, string>(src => src.AccountType))
+                .ForMember(dest => dest.A3, opt => opt.MapFrom<A3TypeResolver, string>(src => src.AccountType))
+                .ForMember(dest => dest.A4, opt => opt.MapFrom<A4TypeResolver, string>(src => src.AccountType));
+                // TODO ...
+
+            // TODO: CreateMap for SummonsForUnlawfulDetainer
             #endregion
 
             // TODO: Figure out what exactly this does 
---
+++ EZSubmitApp.Core/Mapper/Resolvers/AccountTypeResolvers.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EZSubmitApp.Core.Mapper.Resolvers
+{
+    public class AccountTypeResolver : IMemberValueResolver<object, object, string, string>
+    {
+        public string Resolve(object source, object destination, string sourceMember, string destMember, ResolutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class A1TypeResolver : IMemberValueResolver<object, object, string, string>
+    {
+        public string Resolve(object source, object destination, string sourceMember, string destMember, ResolutionContext context)
+        {
+            return sourceMember == "Open Account" ? "X" : "";
+        }
+    }
+
+    public class A2TypeResolver : IMemberValueResolver<object, object, string, string>
+    {
+        public string Resolve(object source, object destination, string sourceMember, string destMember, ResolutionContext context)
+        {
+            return sourceMember == "Contract" ? "X" : "";
+        }
+    }
+    public class A3TypeResolver : IMemberValueResolver<object, object, string, string>
+    {
+        public string Resolve(object source, object destination, string sourceMember, string destMember, ResolutionContext context)
+        {
+            return sourceMember == "Note" ? "X" : "";
+        }
+    }
+    public class A4TypeResolver : IMemberValueResolver<object, object, string, string>
+    {
+        public string Resolve(object source, object destination, string sourceMember, string destMember, ResolutionContext context)
+        {
+            return sourceMember == "Other" ? "X" : "";
+        }
+    }
+}
