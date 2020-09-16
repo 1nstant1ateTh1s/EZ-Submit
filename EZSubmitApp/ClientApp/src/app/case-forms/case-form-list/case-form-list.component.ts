8d7de611add4a50dd957764c5ed1836d24075979
Andrew Brown <ambrown@cityofchesapeake.net>
2020-09-16 20:37:20 +0000

    Add CaseFormList component.

---
+++ EZSubmitApp/ClientApp/src/app/case-forms/case-form-list/case-form-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-case-form-list',
+  templateUrl: './case-form-list.component.html',
+  styleUrls: ['./case-form-list.component.css']
+})
+export class CaseFormListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- EZSubmitApp/ClientApp/src/app/case-forms/case-forms.module.ts
+++ EZSubmitApp/ClientApp/src/app/case-forms/case-forms.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { CaseFormsRoutingModule } from './case-forms-routing.module';
 import { WdFormComponent } from './wd/wd-form/wd-form.component';
+import { CaseFormListComponent } from './case-form-list/case-form-list.component';
 
 
 @NgModule({
-  declarations: [WdFormComponent],
+  declarations: [WdFormComponent, CaseFormListComponent],
   imports: [
     CommonModule,
     CaseFormsRoutingModule
