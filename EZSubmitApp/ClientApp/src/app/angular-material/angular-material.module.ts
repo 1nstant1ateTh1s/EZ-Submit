f45d2233ce42b912d2ebf1d9639e75f0b1786581
Andrew Brown <ambrown@cityofchesapeake.net>
2020-09-17 20:19:36 +0000

    Add AngularMaterial module.

---
+++ EZSubmitApp/ClientApp/src/app/angular-material/angular-material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    // material design
+    MatTableModule
+  ],
+  exports: [
+    // material design
+    MatTableModule
+  ]
+})
+export class AngularMaterialModule { }
